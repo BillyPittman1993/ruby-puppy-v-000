a962f1bc121d66173d0f4e0839a3087c721b4212
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-07-07 13:50:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,14 +10,10 @@ class Dog
   end 
   
   def self.all
-     @@all
-  end 
-  
-  def self.print_all 
-    @@all.each do |name| 
-      puts "#{name}"
-    end
-  end
+     @@all.each do |name| 
+       puts "#{name}"
+     end
+   end 
   
   def self.clear_all 
     @@all.clear
